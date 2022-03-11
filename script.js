5ecae0d6a82d33dc00914592510744b2ec2226a9
Santosh Nayak <santoshnayak1996@gmail.com>
2022-03-11 16:49:58 +0530

    new squat logic

--- script.js
+++ script.js
@@ -4,29 +4,8 @@ let model;
 // let ctx = canvas.getContext("2d");
 let windowHeight = window.outerHeight * 0.4;
 let windowWidth = window.outerWidth - 100;
-var fps = 30;
-
-var thresholdRightKneeAndHipDownDistance = 20;
-var thresholdRightKneeAndHipUpDistance = 30;
 
 var targetCount = 10;
-var isDynamicDistanceSet = false;
-// alert(windowWidth)
-// alert(document.getElementsByClassName("test").offsetWidth);
-// alert(window.outerWidth);
-
-// var thresholdAngle = 130;
-
-// var rightHandCount = 0;
-// var canBeProceedForRightCount = true;
-// var hasRightCountIncreasedOnce = false;
-
-// var leftHandCount = 0;
-// var canBeProceedForLeftCount = true;
-// var hasLeftCountIncreasedOnce = false;
-
-// var isGoalAchieved = false;
-// var goalCount = 5;
 const detectorConfig = {
   modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
 };
@@ -38,15 +17,24 @@ setTimeout(() => {
   video.removeAttribute("controls");
 });
 
-// var upValue = 150;
-// var downValue = 130;
+var tiles_now = 0;
+var tiles_previous = 0;
+var person_height = 0;
+var isHeightAssignedInitially = false;
+var isHeightChangeRequired = false;
+var thresholdSit = 0;
+var initialTime = new Date();
+var currentTime = new Date();
+var positionLocked = false;
+var bFirst = false;
+var upValue = 150;
+var downValue = 130;
 
-// var threshHoldKneeAnkleDistance = 30;
+var threshHoldKneeAnkleDistance = 30;
 let detector;
 
-var canCountIncrease = false;
+var canCountIncrease = true;
 var countValue = 0;
-
 const setupCamera = () => {
   navigator.mediaDevices
     .getUserMedia({
@@ -55,7 +43,7 @@ const setupCamera = () => {
     })
     .then((stream) => {
       video.srcObject = stream;
-      // document.getElementById("goalCount").innerHTML = goalCount;
+      // document.getElementById("targetCount").innerHTML = targetCount;
     });
 };
 
@@ -64,142 +52,111 @@ const detectPose = async () => {
   const poses = await detector.estimatePoses(document.querySelector("video"));
 
   // const predictions = await model.estimateHands(document.querySelector("video"));
-  console.log(poses);
+  // console.log(poses);
 
-  // temporary area
   if (poses.length) {
-    let left_hip = poses[0].keypoints.find((x) => x.name == "left_hip");
-    let left_knee = poses[0].keypoints.find((x) => x.name == "left_knee");
+    let right_shoulder = poses[0].keypoints.find(
+      (x) => x.name == "right_shoulder"
+    );
+    let right_wrist = poses[0].keypoints.find((x) => x.name == "right_wrist");
 
-    let right_hip = poses[0].keypoints.find((x) => x.name == "right_hip");
     let right_knee = poses[0].keypoints.find((x) => x.name == "right_knee");
+    let right_ankle = poses[0].keypoints.find((x) => x.name == "right_ankle");
 
     if (
-      left_hip.score > 0.5 &&
-      left_knee.score > 0.5 &&
-      right_knee.score > 0.5 &&
-      right_hip.score > 0.5 &&
-
       right_shoulder.score > 0.5 &&
-      right_ankle.score > 0.5 
+      right_wrist.score > 0.5 &&
+      right_knee.score > 0.5 &&
+      right_ankle.score > 0.5
     ) {
-      document.getElementById("message").innerHTML =
-        "We are good to count Squats now ";
 
-      document.getElementById("video").style.borderColor = "green";
+      document.getElementById("video").style.borderColor = "blue";
+      // document.getElementById("Ready").innerHTML = "Hold on! We are measuring your distance";
 
-      var rightKneeAndHipDistance = distanceBetweenTwo(
-        right_knee.x,
-        right_hip.x,
-        right_knee.y,
-        right_hip.y
-      );
-
-      var rightShoulderToAnkleDistance =  distanceBetweenTwo(
+      var rightShoulderToAnkleDistance = distanceBetweenTwo(
         right_shoulder.x,
         right_ankle.x,
         right_shoulder.y,
         right_ankle.y
       );
-
-    
-      var tilesAway = (rightShoulderToAnkleDistance - 257.57)/16.036;
-      
-
-      document.getElementById(
-        "rightShoulderToAnkleDistance"
-      ).innerHTML = rightShoulderToAnkleDistance;
-      document.getElementById(
-        "tilesAway"
-      ).innerHTML = tilesAway;
-
-
-
-
-      // document.getElementById("upVal").innerHTML =
-      //   thresholdRightKneeAndHipUpDistance - 5;
       // document.getElementById(
-      //   "downVal"
-      // ).innerHTML = thresholdRightKneeAndHipDownDistance;
-      // document.getElementById("current").innerHTML = rightKneeAndHipDistance;
+      //   "rightShoulderToAnkleDistance"
+      // ).innerHTML = rightShoulderToAnkleDistance;
+
+      // console.log('initialTime',initialTime)
+      // console.log('currentTime',currentTime)
+      var secondDifference = Math.round(
+        (currentTime.getTime() - initialTime.getTime()) / 1000
+      );
+      if (secondDifference > 3) {
+        console.log("Greater than 3 seconds");
+        // document.getElementById("video").style.borderColor = "blue";
+
+        var diff = Math.abs(tiles_now - tiles_previous);
+        if (diff < 1 && !bFirst) {
+          console.log("positionLocked", positionLocked);
+
+          positionLocked = true;
+          bFirst = true;
+          person_height = rightShoulderToAnkleDistance;
+          // document.getElementById("personHeight").innerHTML = person_height;
+          document.getElementById("Ready").innerHTML = "YOU ARE READY TO START";
+          // document.getElementById(
+          //   "isPositionLocked"
+          // ).innerHTML = positionLocked;
+
+          document.getElementById("video").style.borderColor = "green";
 
 
-      // if(rightKneeAndHipDistance <  thresholdRightKneeAndHipUpDistance - 5){
-      //   document.getElementById("message").innerHTML = ' will work';
 
-      // }else{
-      //   document.getElementById("message").innerHTML = ' Not work';
-
-      // }
-
-      //test for dynamic height
-      if (!isDynamicDistanceSet) {
-        setTimeout(()=>{
-          thresholdRightKneeAndHipUpDistance = rightKneeAndHipDistance -5 ;
-          thresholdRightKneeAndHipDownDistance = rightKneeAndHipDistance - 10;     
-        },1500)
-        isDynamicDistanceSet = true;    
+        } else {
+          tiles_previous = tiles_now;
+        }
+        initialTime = currentTime;
+      } else {
+        currentTime = new Date();
+        tiles_now = (257.57 - rightShoulderToAnkleDistance) / 16.036;
       }
 
-      //
+      if (person_height / rightShoulderToAnkleDistance > 1.3) {
+        if (canCountIncrease) {
+          countValue = countValue + 1;
+          canCountIncrease = false;
+          document.getElementById("countValue").innerHTML = countValue;
 
-      // var rightKneeAndAnkleDistance = distanceBetweenTwo(
-      //   right_knee.x,
-      //   right_ankle.x,
-      //   right_knee.y,
-      //   right_ankle.y
-      // );
-
-      // document.getElementById(
-      //   "rightKneeAndHipDistance"
-      // ).innerHTML = rightKneeAndHipDistance;
-
-      if (rightKneeAndHipDistance > thresholdRightKneeAndHipUpDistance) {
-        canCountIncrease = true;
-      }
-      if (
-        rightKneeAndHipDistance <= thresholdRightKneeAndHipDownDistance &&
-        canCountIncrease
-      ) {
-        countValue = countValue + 1;
-        canCountIncrease = false;
-
-        document.getElementById("countValue").innerHTML = countValue;
-
-        if (countValue >= targetCount) {
-          document.getElementById("targetAchievedMessage").innerHTML =
-            "🎇 Target Achieved 🎇";
-          console.log(true);
+          if(countValue >= targetCount){
+            sendMessagetoFlutter(true)
+          }
         }
       }
-    } else {
-      document.getElementById("message").innerHTML =
-        "We are not able to see your whole body";
 
+      if (
+        !canCountIncrease &&
+        rightShoulderToAnkleDistance > person_height - 30
+      ) {
+        canCountIncrease = true;
+      }
+    }else{
       document.getElementById("video").style.borderColor = "red";
+      console.log('red')
+      document.getElementById("Ready").innerHTML = "Oops! You are not in frame";
+
     }
   }
-
-  // ctx.drawImage(video, 0, 0, windowWidth, windowHeight);
-
-  // poses.forEach((eachPose) => {
-  //   ctx.beginPath();
-  //   ctx.lineWidth = "4";
-  //   ctx.strokeStyle = "blue";
-
-  //   ctx.fillStyle = "red";
-  //   eachPose.keypoints.forEach((key, index) => {
-  //     ctx.fillRect(key.x, key.y, 5, 5);
-
-  //   });
-
-  //   ctx.stroke();
-  // });
 };
+
+// rough
+// if tile count is not changing don't change height
+
+start = 100;
+sitting = 100 / 1.4;
 
 setupCamera();
 video.addEventListener("loadeddata", async () => {
   // document.getElementById("video").offsetWidth, document.getElementById("video").offsetHeight
+
+  // document.getElementById("isPositionLocked").innerHTML = positionLocked;
+  // document.getElementById("initialTime").innerHTML = initialTime;
 
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
@@ -208,12 +165,6 @@ video.addEventListener("loadeddata", async () => {
   }
   document.getElementById("targetCount").innerHTML = targetCount;
 
-  console.log("queryString", targetCount);
-
-  // canvas.width = document.getElementById("video").offsetWidth;
-  // canvas.height = document.getElementById("video").offsetHeight;
-  // canvas.setAttribute("width", windowWidth);
-  // canvas.setAttribute("height", windowHeight);
   detector = await poseDetection.createDetector(
     poseDetection.SupportedModels.MoveNet,
     detectorConfig
@@ -222,10 +173,7 @@ video.addEventListener("loadeddata", async () => {
   document.getElementById("loadingText").innerHTML =
     "Please stand in front of camera";
 
-  // document.getElementById("upscoreThreshold").innerHTML =upValue;
-  // document.getElementById("downscoreThreshold").innerHTML =downValue;
-
-  setInterval(detectPose, fps);
+  setInterval(detectPose, 30);
 });
 
 function sendMessagetoFlutter(value) {
